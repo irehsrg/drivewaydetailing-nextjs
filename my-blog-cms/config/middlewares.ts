09bf1364072616f516744cca762ec65b716d51e7
Irehsrg <ajoines03@gmail.com>
2025-04-05 17:30:05 -0500

    Strapi middleware fixes

--- my-blog-cms/config/middlewares.ts
+++ my-blog-cms/config/middlewares.ts
@@ -1,27 +1,21 @@
-export default [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
-
-
-module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      enabled: true,
       headers: '*',
       origin: [
         'http://localhost:3000', 
         'https://drivewaydetailing-nextjs-production.up.railway.app'
       ]
     }
-  }
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
